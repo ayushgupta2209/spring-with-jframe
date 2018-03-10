aab5e31ab27e21b8b2d790baf50e877cd2c948d2
Ayush <aygupta@adobe.com>
2018-03-10 23:41:51 +0530

    Changes for creating jar

--- src/main/java/com/example/javaui/JavauiApplication.java
+++ src/main/java/com/example/javaui/JavauiApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 public class JavauiApplication implements CommandLineRunner {
 
@@ -20,13 +18,6 @@ public class JavauiApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		String[] beans = appContext.getBeanDefinitionNames();
-		Arrays.sort(beans);
-		for (String bean : beans) {
-			System.out.println(bean);
-		}
-
 		java.awt.EventQueue.invokeLater(() -> appContext.getBean(InputNameForm.class).setVisible(true));
 	}
 
